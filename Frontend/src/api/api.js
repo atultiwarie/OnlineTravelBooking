17e73795d31056df73415ef061014a2bdd70fb77
atultiwarie <atultiwari4767@gmail.com>
2025-09-24 21:44:22 +0530

    started frontend

---
+++ Frontend/src/api/api.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+const API = axios.create({
+    baseURL:'http://localhost:3000/api'
+})
+
+// tokens to requests
+
+API.interceptors.request.use((req)=>{
+    const token = localStorage.getItem('token')
+    if(token){
+        req.headers.Authorization = `Bearer ${token}`
+    }
+    return req
+})
+
+API.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response && err.response.status === 401) {
+      // auto logout if token expired
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
+
+
+// Auth
+
+export const registerUser = (data)=> API.post('/auth/register',data)
+export const loginUser = (data)=> API.post('/auth/login',data)
+
+// Hotels
+export const fetchHotels = (params)=> API.get('/hotels', { params })
+
+// Cart
+export const addToCart = (data)=> API.post('/cart',data)
+export const getCart =()=>API.get('/cart')
+export const removeFromCart = (id)=>API.delete(`/cart/${id}`)
+
+// Bookings
+export const checkoutBooking = (data) => API.post("/checkout", data);
